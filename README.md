7a4afcfb02c26e1d7094e614eb29b7ddd5bc7d9f
davy52 <dkliminskip@gmail.com>
2022-05-11 21:48:44 +0200

    the great purge

---
+++ README.md
@@ -0,0 +1,2 @@
+# interfesy_class_project
+System control visualization for class project
---
+++ animacja/mat.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib as mp
+import matplotlib.pyplot as plt
+
+from scipy.integrate import odeint
+
+
+
+c_S = 2 		#m^2
+c_k = 0.5		#stała przepływu
+
+#nastawy_PID
+c_Kp = 5
+c_Ki = 3
+c_Kd = 0.001
+
+sat = [0, 7] 		#saturacja - graniczne wartości wymuszenia
+#end-of-nastawy_PID
+
+#warunki_początkowe
+y0 = 0 						#h0
+
+#end-of-warunki_początkowe
+
+t = np.arange(0, 10, 1e-6)
+
+
+#zmienne_regulatora?
+vals = [5.0, 7.6, 2]
+t_traj = [1.0, 3.0, 6.0]
+
+#end-of-zmienne_reg
+
+
+[e_old, t_old, I, F_old] = [0, 0, 0, 0]
+i = 0
+traj = [vals, [*t_traj, 1000.0]]
+
+tt = [0]
+FF = [0]
+ee = [0]
+
+def model(y, t):
+	global c_Kp, c_Ki, c_Kd, c_k, c_S
+	global e_old, t_old, I, F_old
+	global tt, FF, ee, sat, i, traj
+	dydt = np.zeros_like(y)
+	
+	if traj[1][i+1] < t:
+		i += 1
+	hz = traj[0][i]
+
+	if t-t_old > 0.0001:
+		e = hz - y 								# uchyb
+		dedt = (e - e_old)/(t - t_old)			# rozniczka
+		I = I + (e) * (t - t_old)		# calka
+		F = c_Kp * e + c_Ki * I + c_Kd * dedt	# sterowanie
+
+
+		if F > sat[1]:
+			F = sat[1]
+		elif F < sat[0]:
+			F = sat[0]
+
+		[e_old, t_old, F_old] = [e, t, F]
+		tt.append(t)
+		FF.append(F)
+		ee.append(e)
+
+	else:
+		F = F_old
+
+	dydt = (1/c_S)*F - (c_k/c_S)*np.sqrt(y)
+	return dydt
+
+
+
+y = odeint(model, y0, t)
+
+
+#dtmax = max([ t2-t1 for t1,t2 in zip(tt[:], tt[1:]) if t1 < 0.6])
+#print(dtmax)
+
+plt.subplot(211)
+plt.plot(t, y, label="h")
+plt.plot(tt, ee, label="uchyb")
+plt.stairs(traj[0], traj[1], label="trajektoria")
+plt.xlim((t[0], t[-1]))
+plt.legend(loc="upper right")
+plt.grid()
+
+plt.subplot(212)
+plt.step(tt, FF, label="wymuszenie")
+plt.xlim((t[0], t[-1]))
+plt.legend(loc="upper right")
+plt.grid()
+
+plt.show()
+
